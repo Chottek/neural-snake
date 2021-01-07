c254a148000df9e4b21a3355831fdb10d06f05e9
Chottek <49697145+Chottek@users.noreply.github.com>
2021-01-07 17:54:21 +0100

    Guess, title should also be static final

--- src/main/java/pl/fox/neuralsnake/Launcher.java
+++ src/main/java/pl/fox/neuralsnake/Launcher.java
@@ -2,11 +2,12 @@ package pl.fox.neuralsnake;
 
 public class Launcher {
 
+    public static final String TITLE = "NeuralSnake";
     public static final int WIDTH = 700;
     public static final int HEIGHT = 400;
 
     public static void main(String[] args) {
-        new Game("NeuralSnake", WIDTH, HEIGHT).start();
+        new Game(TITLE, WIDTH, HEIGHT).start();
     }
 
 
