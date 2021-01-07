323a81bb4eaa4337e1fd722d1091ed39bc595f47
Chottek <49697145+Chottek@users.noreply.github.com>
2021-01-07 11:35:40 +0100

    Added Display with JFrame

--- src/main/java/pl/fox/neuralsnake/display/Display.java
+++ src/main/java/pl/fox/neuralsnake/display/Display.java
@@ -1,4 +1,45 @@
 package pl.fox.neuralsnake.display;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Display {
+
+    private JFrame frame;
+    private Canvas canvas;
+
+    private final String title;
+    private final int width;
+    private final int height;
+
+    public Display(String title, int width, int height){
+        this.title = title;
+        this.width = width;
+        this.height = height;
+
+        createDisplay();
+    }
+
+    private void createDisplay(){
+        frame = new JFrame(title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        canvas = new Canvas();
+        canvas.setPreferredSize(new Dimension(width, height));
+        canvas.setBackground(Color.BLACK);
+        canvas.setFocusable(false);
+
+        frame.add(canvas);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+    }
+
+    public Canvas getCanvas(){ return canvas; }
+
+    public JFrame getFrame(){ return frame; }
+
+
+
 }
