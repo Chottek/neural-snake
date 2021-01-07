1e1d5a4e675c81de9c04821ae1f1f058d58a3e37
Chottek <49697145+Chottek@users.noreply.github.com>
2021-01-07 16:34:24 +0100

    Implemented generateFood() method, added iterator to update and render

--- src/main/java/pl/fox/neuralsnake/world/Field.java
+++ src/main/java/pl/fox/neuralsnake/world/Field.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.awt.*;
-import java.util.Random;
 
 public class Field {
 
@@ -13,25 +12,34 @@ public class Field {
     private static final int RAND_POS_X = World.B_WIDTH / 10;
     private static final int RAND_POS_Y = World.B_HEIGHT / 10;
 
-    private final Random random;
+    private final java.util.List<Apple> apples;
 
     public Field(){
-        random = new Random();
+        apples = new java.util.ArrayList<>();
 
         generateFood();
     }
 
     private void generateFood(){
+        int rand, x, y;
+        for(int i = 0; i < World.FOOD_COUNT; i++){
+            rand = (int) (Math.random() * RAND_POS_X);
+            x = (rand * World.MODULE_SIZE);
 
+            rand = (int) (Math.random() * RAND_POS_Y);
+            y = (rand * World.MODULE_SIZE);
+
+            apples.add(new Apple(x, y));
+        }
+        LOG.info("Initialized a Field of {} apples", apples.size());
     }
 
     public void update(){
-
+        apples.forEach(Apple::update);
     }
 
     public void render(Graphics2D g){
-
+        apples.forEach(a -> a.render(g));
     }
-
 
 }
